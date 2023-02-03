b0680559e5b1aab18ae7f39e37707e1fbcaa1d8f
Andrew Fedorov <clash2clans1one@gmail.com>
2023-02-03 15:35:16 +0200

    correct time format

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import os
 import moviepy.editor as mp
 import time
 import re
+import math
 
 
-def download_video(Url, Download_folder):
-    yt = YouTube(Url)
-    title = re.sub(r'[\\:*?"<>]', '', yt.title)
+def download_video(url, Download_folder):
+    yt = YouTube(url)
+    title = re.sub(r'[\\:*?"<>/]', '', yt.title)
     tmp_audio_file = 'unknown_mp3.mp4'
     tmp_video_file = 'unknown_mp4.mp4'
 
@@ -23,7 +24,7 @@ def download_video(Url, Download_folder):
 
     # merge two streams
     clip = mp.VideoFileClip(tmp_video_file).subclip()
-    clip.write_videofile(title + ".mp4", audio=tmp_audio_file, logger=None, )
+    clip.write_videofile(title + ".mp4", audio=tmp_audio_file, logger=None)
 
     # clear excess
     os.remove(tmp_audio_file)
@@ -38,8 +39,12 @@ if __name__ == "__main__":
         time_video_start = time.time()
         print(Fore.YELLOW + f"Downloading video...")
         download_video(url, download_folder)
-        total_video_time = format(((time.time() - time_video_start) / 60), '.2f')
-        print(Fore.GREEN + f"Video download completed in {total_video_time}m", end='\n\n')
+
+        difference = (time.time() - time_video_start)/60
+        total_video_min = int(difference)
+        total_video_sec = math.ceil((difference - total_video_min) * 60)
+
+        print(Fore.GREEN + f"Video download completed in {total_video_min}m {total_video_sec}s", end='\n\n')
     elif "playlist?" in url:  # download all videos from playlist
         p = Playlist(url)
         playlist_title = p.title
@@ -53,18 +58,28 @@ if __name__ == "__main__":
         for video in p.videos:
             time_video_start = time.time()
             print(Fore.YELLOW + f"Downloading video {video.title}...")
-            try:
-                download_video(video.watch_url, download_folder)
-                progress += 1
-                total_video_time = format(((time.time() - time_video_start) / 60), '.2f')
-                print(Fore.GREEN + f"Video download completed in {total_video_time}m")
-                print(Fore.GREEN + f"Total progress: {progress}/{total_count}", end='\n\n')
-            except BaseException:
-                print(Fore.RED + "! Downloading error !")
-                continue
 
-        total_playlist_time = format((time.time() - start_time) / 60, '.2f')
-        print(f"Video download completed in {total_playlist_time}m")
+            # try:
+
+            download_video(video.watch_url, download_folder)
+            progress += 1
+
+            difference = (time.time() - time_video_start) / 60
+            total_video_min = int(difference)
+            total_video_sec = math.ceil((difference - total_video_min) * 60)
+
+            print(Fore.GREEN + f"Video download completed in {total_video_min}m {total_video_sec}s")
+            print(Fore.GREEN + f"Total progress: {progress}/{total_count}", end='\n\n')
+
+            # except PytubeError:
+            #     print(Fore.RED + "! Downloading error !")
+            #     continue
+
+        difference = (time.time() - start_time) / 60
+        total_video_min = int(difference)
+        total_video_sec = math.ceil((difference - total_video_min) * 60)
+
+        print(f"Playlist download completed in {total_video_min}m {total_video_sec}s")
 
 
 
