17fdf0d6b4ce09d8ca4f0c8da21641058bdeb6d6
Andreww <clash2clans1one@gmail.com>
2023-02-03 22:14:56 +0200

    another merge method that is much faster(using ffmpeg.exe than moviepy lib)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pytube import YouTube, Playlist, Channel
 from colorama import init, Fore
 import os
-import moviepy.editor as mp
+#import moviepy.editor as mp
 import time
 import re
 import math
@@ -9,26 +9,44 @@ import math
 
 def download_video(url, Download_folder):
     yt = YouTube(url)
-    title = re.sub(r'[\\/:*?"<>/]', '', yt.title)
+    title = re.sub(r'[\\:*?"<>/]', '', yt.title)
+    downloading_sources = time.time()
     tmp_audio_file = 'unknown_mp3.mp4'
     tmp_video_file = 'unknown_mp4.mp4'
 
+
+
     # download audio stream
     yt.streams.filter(mime_type="audio/mp4").first().download(Download_folder, tmp_audio_file)
-
-    os.chdir(Download_folder)
 
     # download video stream
     yt.streams.filter(mime_type="video/mp4").order_by('resolution')\
               .desc().first().download(Download_folder, filename=tmp_video_file)
 
+    difference = (time.time() - downloading_sources) / 60
+    min = int(difference)
+    sec = math.ceil((difference - min) * 60)
+    print(Fore.GREEN + f"[DEBUG] Download sources completed in {min}m {sec}s")
+
+    merge_time = time.time()
+
     # merge two streams
-    clip = mp.VideoFileClip(tmp_video_file).subclip()
-    clip.write_videofile(title + ".mp4", audio=tmp_audio_file, logger=None)
+
+    cmd = f"ffmpeg -i {Download_folder}/{tmp_audio_file} -i {Download_folder}/{tmp_video_file} -c:v copy {Download_folder}/123result123.mp4 -v error"
+    os.system(cmd)
 
     # clear excess
+
+    os.chdir(Download_folder)
+    os.rename(f"123result123.mp4", f"{title}.mp4")
+
     os.remove(tmp_audio_file)
     os.remove(tmp_video_file)
+
+    difference = (time.time() - merge_time) / 60
+    min = int(difference)
+    sec = math.ceil((difference - min) * 60)
+    print(Fore.GREEN + f"[DEBUG] Merge completed in {min}m {sec}s")
 
 
 if __name__ == "__main__":
@@ -37,7 +55,7 @@ if __name__ == "__main__":
 
     if 'watch?' in url:  # download video
         time_video_start = time.time()
-        print(Fore.YELLOW + f"Downloading video...")
+        print(Fore.YELLOW + f"Downloading video")
         download_video(url, download_folder)
 
         difference = (time.time() - time_video_start)/60
@@ -47,7 +65,7 @@ if __name__ == "__main__":
         print(Fore.GREEN + f"Video download completed in {total_video_min}m {total_video_sec}s", end='\n\n')
     elif "playlist?" in url:  # download all videos from playlist
         p = Playlist(url)
-        playlist_title = re.sub(r'[\\/:*?"<>/]', '', p.title)
+        playlist_title = p.title
         start_time = time.time()
         print(Fore.YELLOW + f"Start downloading playlist {playlist_title}")
         download_folder = f"{download_folder}/{playlist_title}"
