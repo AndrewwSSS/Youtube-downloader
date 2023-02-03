52bc71ae7ddfe573524741351f006d6b942bae37
Andreww <clash2clans1one@gmail.com>
2023-02-03 22:51:34 +0200

    renaming, verification download folder

--- main.py
+++ main.py
@@ -1,71 +1,74 @@
 from pytube import YouTube, Playlist, Channel
 from colorama import init, Fore
 import os
-#import moviepy.editor as mp
+# import moviepy.editor as mp
 import time
 import re
 import math
 
 
-def download_video(url, Download_folder):
-    yt = YouTube(url)
-    title = re.sub(r'[\\:*?"<>/]', '', yt.title)
+def download_video(video_url, download_folder):
+    yt = YouTube(video_url)
+    title = re.sub(r'[\\:*?"<>/|]', '', yt.title)
     downloading_sources = time.time()
-    tmp_audio_file = 'unknown_mp3.mp4'
-    tmp_video_file = 'unknown_mp4.mp4'
-
-
+    audio_file = 'unknown1.mp4'
+    video_file = 'unknown2.mp4'
 
     # download audio stream
-    yt.streams.filter(mime_type="audio/mp4").first().download(Download_folder, tmp_audio_file)
+    yt.streams.filter(mime_type="audio/mp4").first().download(download_folder, audio_file)
 
     # download video stream
-    yt.streams.filter(mime_type="video/mp4").order_by('resolution')\
-              .desc().first().download(Download_folder, filename=tmp_video_file)
+    yt.streams.filter(mime_type="video/mp4").order_by('resolution') \
+        .desc().first().download(download_folder, filename=video_file)
 
-    difference = (time.time() - downloading_sources) / 60
-    min = int(difference)
-    sec = math.ceil((difference - min) * 60)
-    print(Fore.GREEN + f"[DEBUG] Download sources completed in {min}m {sec}s")
+    time_difference = (time.time() - downloading_sources) / 60
+    minutes = int(time_difference)
+    seconds = math.ceil((time_difference - minutes) * 60)
+    print(Fore.GREEN + f"[DEBUG] Download sources completed in {minutes}m {seconds}s")
 
     merge_time = time.time()
 
     # merge two streams
 
-    cmd = f"ffmpeg -i {Download_folder}/{tmp_audio_file} -i {Download_folder}/{tmp_video_file} -c:v copy {Download_folder}/123result123.mp4 -v error"
+    cmd = f"ffmpeg -i {download_folder}/{audio_file} -i {download_folder}/{video_file} -c:v copy {download_folder}/123result123.mp4 -v error"
     os.system(cmd)
 
     # clear excess
 
-    os.chdir(Download_folder)
+    os.chdir(download_folder)
     os.rename(f"123result123.mp4", f"{title}.mp4")
 
-    os.remove(tmp_audio_file)
-    os.remove(tmp_video_file)
+    os.remove(audio_file)
+    os.remove(video_file)
 
-    difference = (time.time() - merge_time) / 60
-    min = int(difference)
-    sec = math.ceil((difference - min) * 60)
-    print(Fore.GREEN + f"[DEBUG] Merge completed in {min}m {sec}s")
+    time_difference = (time.time() - merge_time) / 60
+    minutes = int(time_difference)
+    seconds = math.ceil((time_difference - minutes) * 60)
+    print(Fore.GREEN + f"[DEBUG] Merge completed in {minutes}m {seconds}s")
 
 
 if __name__ == "__main__":
     url = input("Enter video or playlist URL: ")
-    download_folder = input("Enter download path: ")
+    download_folder = input("Enter download folder: ")
+
+    if not os.path.exists(download_folder):
+        print(Fore.RED + "Folder not found!")
+        input()
+        exit()
 
     if 'watch?' in url:  # download video
-        time_video_start = time.time()
+        time_start = time.time()
         print(Fore.YELLOW + f"Downloading video")
         download_video(url, download_folder)
 
-        difference = (time.time() - time_video_start)/60
+        difference = (time.time() - time_start) / 60
         total_video_min = int(difference)
         total_video_sec = math.ceil((difference - total_video_min) * 60)
 
         print(Fore.GREEN + f"Video download completed in {total_video_min}m {total_video_sec}s", end='\n\n')
     elif "playlist?" in url:  # download all videos from playlist
         p = Playlist(url)
-        playlist_title = p.title
+        playlist_title = re.sub(r'[\\:*?"<>/|]', '', p.title)
         start_time = time.time()
         print(Fore.YELLOW + f"Start downloading playlist {playlist_title}")
         download_folder = f"{download_folder}/{playlist_title}"
@@ -74,7 +77,7 @@ if __name__ == "__main__":
         progress = 0
         total_count = len(p.video_urls)
         for video in p.videos:
-            time_video_start = time.time()
+            time_start = time.time()
             print(Fore.YELLOW + f"Downloading video {video.title}...")
 
             # try:
@@ -82,7 +85,7 @@ if __name__ == "__main__":
             download_video(video.watch_url, download_folder)
             progress += 1
 
-            difference = (time.time() - time_video_start) / 60
+            difference = (time.time() - time_start) / 60
             total_video_min = int(difference)
             total_video_sec = math.ceil((difference - total_video_min) * 60)
 
@@ -99,13 +102,6 @@ if __name__ == "__main__":
 
         print(f"Playlist download completed in {total_video_min}m {total_video_sec}s")
 
-
-
-
-
-
-
-
 # elif('@' in url):  #download all videos from channel
 #     c = Channel(url)
 #     channel_title = c.title
@@ -117,11 +113,11 @@ if __name__ == "__main__":
 #     progress = 0
 #     total_count = len(c.video_urls)
 #     for video in c.videos:
-#         time_video_start = time.time()
+#         time_start = time.time()
 #         print(f"Downloading video {video.title}...")
 #         download_video(video.watch_url, download_folder)
 #         progress += 1
-#         total_video_time = format(((time.time() - time_video_start)/60), '.2f')
+#         total_video_time = format(((time.time() - time_start)/60), '.2f')
 #         print(f"Downloading video complete in {total_video_time}m", end='\n')
 #         print(f"Current progress: {progress}/{total_count}")
 #
