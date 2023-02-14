30333c8f0b3c20f1f3378e2bb2117e85d524b030
Andreww <clash2clans1one@gmail.com>
2023-02-14 14:37:46 +0200

    fixes, exception handling of unavailiable video

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pytube import YouTube, Playlist
-from  pytube.exceptions import PytubeError
+from pytube.exceptions import PytubeError, VideoUnavailable
 from colorama import Fore
 from win32com.shell import shell, shellcon
 from threading import *
@@ -8,67 +8,53 @@ import time
 import re
 import math
 import uuid
+from datetime import datetime
 
 
-def aboba(video_url, download_folder):
-    time_start = time.time()
+def download_video(video_url: str, folder: str):
+    start = time.time()
 
     while True:
         try:
-            # print(Fore.YELLOW + f"Downloading video {video_url}")
-            download_video(video_url, download_folder)
+            yt = YouTube(video_url)
+
+            title = re.sub(r'[\\:*?"<>/|]', '', yt.title)
+            downloading_sources = time.time()
+            audio_file = f'{uuid.uuid1()}.mp4'
+            video_file = f'{uuid.uuid1()}.mp4'
+
+            # download audio stream
+            yt.streams.filter(mime_type="audio/mp4").first().download(folder, audio_file)
+
+            # download video stream
+            yt.streams.filter(mime_type="video/mp4").order_by('resolution') \
+                .desc().first().download(folder, filename=video_file)
+
+            # merge two streams
+            file_name = uuid.uuid1()
+            cmd = f"ffmpeg -i {folder}/{audio_file} -i {folder}/{video_file} -c:v copy {folder}/{file_name}.mp4 -v error"
+            os.system(cmd)
+
+            # clear excess
+            os.rename(f"{folder}/{file_name}.mp4", f"{folder}/{title}.mp4")
+
+            os.remove(f"{folder}/{audio_file}")
+            os.remove(f"{folder}/{video_file}")
+
+            print(Fore.LIGHTYELLOW_EX + f"[DEBUG] " + title)
+            break
+        except VideoUnavailable:
+            print(Fore.RED + f'Video {video_url} is unavaialable, skipping.')
             break
         except PytubeError:
-            # print(Fore.RED + "[Debug] Download error. Retry...", end='\n\n')
             continue
 
-    difference = (time.time() - time_start) / 60
-    min = int(difference)
-    sec = math.ceil((difference - min) * 60)
+
+    diff = (time.time() - start) / 60
+    min = int(diff)
+    sec = math.ceil((diff - min) * 60)
 
     print(Fore.GREEN + f"Video download completed in {min}m {sec}s")
-
-
-def download_video(video_url, download_folder):
-
-    yt = YouTube(video_url)
-
-    title = re.sub(r'[\\:*?"<>/|]', '', yt.title)
-    downloading_sources = time.time()
-    audio_file = f'{uuid.uuid1()}.mp4'
-    video_file = f'{uuid.uuid1()}.mp4'
-
-    # download audio stream
-    yt.streams.filter(mime_type="audio/mp4").first().download(download_folder, audio_file)
-
-    # download video stream
-
-    yt.streams.filter(mime_type="video/mp4").order_by('resolution') \
-        .desc().first().download(download_folder, filename=video_file)
-    time_difference = (time.time() - downloading_sources) / 60
-
-    minutes = int(time_difference)
-    seconds = math.ceil((time_difference - minutes) * 60)
-    # print(Fore.LIGHTYELLOW_EX + f"[DEBUG] Download sources completed in {minutes}m {seconds}s")
-
-    merge_time = time.time()
-
-    # merge two streams
-    file_name = uuid.uuid1()
-    cmd = f"ffmpeg -i {download_folder}/{audio_file} -i {download_folder}/{video_file} -c:v copy {download_folder}/{file_name}.mp4 -v error"
-    os.system(cmd)
-
-    # clear excess
-    os.rename(f"{download_folder}/{file_name}.mp4", f"{download_folder}/{title}.mp4")
-
-    os.remove(f"{download_folder}/{audio_file}")
-    os.remove(f"{download_folder}/{video_file}")
-
-    time_difference = (time.time() - merge_time) / 60
-    minutes = int(time_difference)
-    seconds = math.ceil((time_difference - minutes) * 60)
-    print(Fore.LIGHTYELLOW_EX + f"[DEBUG] " + title)
-    # print(Fore.LIGHTYELLOW_EX + f"[DEBUG] Merge completed in {minutes}m {seconds}s")
 
 
 if __name__ == "__main__":
@@ -93,9 +79,9 @@ if __name__ == "__main__":
                 print(Fore.RED + "[Debug] Download error. Retry...", end='\n\n')
                 continue
 
-        difference = (time.time() - time_start) / 60
-        min = int(difference)
-        sec = math.ceil((difference - min) * 60)
+        diff = (time.time() - time_start) / 60
+        min = int(diff)
+        sec = math.ceil((diff - min) * 60)
 
         print(Fore.GREEN + f"Video download completed in {min}m {sec}s", end='\n\n')
         input()
@@ -119,18 +105,20 @@ if __name__ == "__main__":
             if total_count - progress <= threads_count:
                 threads = []
                 for vid in p.video_urls[progress:total_count]:
-                    threads.append(Thread(target=aboba, args=(vid, download_folder)))
-                for thread in threads:
+                    thread = Thread(target=download_video, args=(vid, download_folder))
+                    threads.append(thread)
                     thread.start()
+
                 for thread in threads:
                     thread.join()
                 break
             else:
                 threads = []
                 for vid in p.video_urls[progress:progress+threads_count]:
-                    threads.append(Thread(target=aboba, args=(vid, download_folder)))
-                for thread in threads:
+                    thread = Thread(target=download_video, args=(vid, download_folder))
+                    threads.append(thread)
                     thread.start()
+
                 for thread in threads:
                     thread.join()
                 progress += threads_count
